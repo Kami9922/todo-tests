30105a8e4c3b2045538eea0fda1058c340e9a5ca
kami <kami-vi@mail.ru>
2025-08-20 13:39:51 +0400

    test: add tests for task list

--- spec/unit/List.spec.tsx
+++ spec/unit/List.spec.tsx
@@ -1,41 +1,82 @@
-import { render, screen } from "@testing-library/react";
-import { List } from "src/components/List";
+import { screen, waitFor } from '@testing-library/react'
+import { List } from 'src/components/List'
+import ue from '@testing-library/user-event'
+import { NewTaskBar } from 'src/modules/NewTaskBar'
+import { renderWithProviders } from '../utils/renderWithProviders'
 
-it("отображение списка задач", () => {
-  const onDelete = jest.fn();
-  const onToggle = jest.fn();
+const userEvent = ue.setup({
+	advanceTimers: jest.advanceTimersByTime,
+})
+describe('Список задач', () => {
+	it('отображение списка задач', () => {
+		const onDelete = jest.fn()
+		const onToggle = jest.fn()
 
-  const items: Task[] = [
-    {
-      id: "1",
-      header: "купить хлеб",
-      done: false,
-    },
-    {
-      id: "2",
-      header: "купить молоко",
-      done: false,
-    },
-    {
-      id: "3",
-      header: "выгулять собаку",
-      done: true,
-    },
-  ];
+		const items: Task[] = [
+			{
+				id: '1',
+				header: 'купить хлеб',
+				done: false,
+			},
+			{
+				id: '2',
+				header: 'купить молоко',
+				done: false,
+			},
+			{
+				id: '3',
+				header: 'выгулять собаку',
+				done: true,
+			},
+		]
 
-  const { rerender, asFragment } = render(
-    <List items={items} onDelete={onDelete} onToggle={onToggle} />
-  );
-  const firstRender = asFragment();
-  
-  items.pop();
+		renderWithProviders(
+			<List
+				items={items}
+				onDelete={onDelete}
+				onToggle={onToggle}
+			/>
+		)
 
-  rerender(<List items={items} onDelete={onDelete} onToggle={onToggle} />);
-  const secondRender = asFragment();
+		items.pop()
+	})
 
-  expect(firstRender).toMatchDiffSnapshot(secondRender);
-});
+	it('Cписок не может содержать больше 10 невыполненных задач', async () => {
+		const onDelete = jest.fn()
+		const onToggle = jest.fn()
+		const onAdd = jest.fn()
 
-it("Список содержит не больше 10 невыполненных задач", () => {
+		const initialItems: Task[] = Array.from({ length: 10 }, (_, i) => ({
+			id: `${i + 1}`,
+			header: `Задача ${i + 1}`,
+			done: false,
+		}))
 
-});
+		renderWithProviders(
+			<>
+				<NewTaskBar />
+				<List
+					items={initialItems}
+					onDelete={onDelete}
+					onToggle={onToggle}
+				/>
+			</>
+		)
+
+		const elInput = screen.getByRole('textbox')
+		const addButtonEl = screen.getByTestId('add-button')
+		let checkboxes = screen.getAllByRole('checkbox', { checked: false })
+
+		expect(checkboxes.length).toBe(10)
+
+		expect(addButtonEl).toBeDisabled()
+
+		await userEvent.type(elInput, 'Новая задача 11')
+		await userEvent.click(addButtonEl)
+
+		checkboxes = screen.getAllByRole('checkbox', { checked: false })
+		expect(checkboxes.length).toBe(10)
+		expect(onAdd).not.toHaveBeenCalled()
+		expect(screen.queryByText('Новая задача 11')).not.toBeInTheDocument()
+	})
+})
